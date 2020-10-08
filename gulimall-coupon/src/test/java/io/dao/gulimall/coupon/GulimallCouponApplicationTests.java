f8f03eb7808629310ef252306762272bbc8a3dc0
LIM YI CHWEN <ben.lim@solderfieldgroup.com>
2020-10-08 17:16:21 +0800

    create project initial structure

---
+++ gulimall-coupon/src/test/java/io/dao/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dao.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
